95b46afbf85feb5ad190d9d9ca7f49e3728e736e
Kęstutis Skrebė <kestutis.julius@gmail.com>
2022-06-04 09:51:38 +0300

    splAutoload class !

--- 00_classExtends/index.php
+++ 00_classExtends/index.php
@@ -1,7 +1,7 @@
 <?php
-require __DIR__.'./Senelis.php';
-require __DIR__.'./Tevas.php';
-require __DIR__.'./Vaikas.php';
+spl_autoload_register(function ($class){
+    require __DIR__.'/'.$class.'.php';
+});
 
 $vaikas = new Vaikas();
 $vaikas->betvarke();
